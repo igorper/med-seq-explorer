4a4b74e541eae7388c93cba79aa3b3856c72e10f
Igor Pernek <igor@pernek.net>
2014-07-15 08:21:22 -0700

    Some small fixes.

--- src/main/scala/SearchTopicSequenceGenerator.scala
+++ src/main/scala/SearchTopicSequenceGenerator.scala
@@ -20,9 +20,10 @@ object SearchTopicSequenceGenerator {
 		val conf = ConfigFactory.load
 		val prepFolder = conf.getString("outputFolder")
 		// search and topic sequence non interupted (only one search in each sequence)
-		val output = conf.getString("search_tseq_nint_")
+		val output = conf.getString("search_tseq_nint")
 		val minSeq = conf.getInt("minSequence")
 		val maxSeq = conf.getInt("maxSequence")
+		val maxResults = conf.getInt("maxNonFilteredResults")
 
 
 		val config = new SparkConf()
@@ -31,7 +32,7 @@ object SearchTopicSequenceGenerator {
 		val sc = new SparkContext(config)
 
 		// read data
-		val file = sc.textFile(input)
+		val file = sc.textFile(prepFolder + "*")
 
 		val countSeperate = file.map(m => m.split("\t")).map(m => (m.slice(1, m.size), m(0).toInt))
 
@@ -45,17 +46,17 @@ object SearchTopicSequenceGenerator {
 	 	val withoutPref = ordered.map(m =>(m._1, m._2.split("\t").map(i=>i.substring(2,i.size)))).map(m => (m._1, m._2(0), m._2.slice(1, m._2.size))).cache
 
 
-	 	for(l <- minSequence to (maxSequence - 1)) {
+	 	for(l <- minSeq to (maxSeq - 1)) {
 	 		// store only those that occure more than once
-	 		val toSave = withoutPref.filter(_._1 > 1 && _._3.size == l).map(x => x._1 + ", (" + x._2 + "): " + x._3.mkString(" --> "))
-	 		toSave.coalesce(1).saveAsTextFile(output + l)
+	 		val toSave = withoutPref.filter(f => f._1 > 1 && f._3.size == l).map(x => x._1 + ", (" + x._2 + "): " + x._3.mkString(" --> "))
+	 		sc.makeRDD(toSave.take(maxResults)).coalesce(1).saveAsTextFile(output + l)
 	 	}
 
 		// stop timing execution
 		val t1 = System.nanoTime()
 
 		println("##########")
-		printf("Processed '%s' in %d seconds.\n", input, (t1 - t0)/1000000000)
+		printf("Processed '%s' in %d seconds.\n", prepFolder, (t1 - t0)/1000000000)
 		println("##########")
 	}
 }
